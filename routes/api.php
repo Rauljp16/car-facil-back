7508dc8541974b5e4362bd0df4770ea1b74e36b9
Rauljp16 <raul_astra16@hotmail.com>
2025-02-10 11:51:34 +0100

    feat: create api.php

--- routes/api.php
+++ routes/api.php
@@ -9,4 +9,5 @@ Route::post('/coches', [CocheController::class, 'store']);
 Route::get('/coches/{id}', [CocheController::class, 'show']);
 Route::put('/coches/{id}', [CocheController::class, 'update']);
 Route::delete('/coches/{id}', [CocheController::class, 'destroy']);
+// Route::delete('/coches/{id}', [CocheController::class, 'destroy']);
 
