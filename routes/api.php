ba5522118c3287be83863224f1cb9dbd32d87209
Rauljp16 <raul_astra16@hotmail.com>
2025-02-12 16:38:07 +0100

    feat: Implementacion de middleware en las rutas

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,19 @@ use App\Http\Controllers\CocheController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+// Rutas públicas (no requieren autenticación)
+Route::post("login", [AuthController::class, 'login']);
 
-Route::get('/coches', [CocheController::class, 'index']);
-Route::post('/coches', [CocheController::class, 'store']);
-Route::get('/coches/{id}', [CocheController::class, 'show']);
-Route::put('/coches/{id}', [CocheController::class, 'update']);
-Route::delete('/coches/{id}', [CocheController::class, 'destroy']);
-Route::post("register",[AuthController::class,'register']);
-Route::post("login",[AuthController::class,'login']);
-Route::post("logout",[AuthController::class,'logout']);
+// Rutas protegidas por autenticación JWT
+Route::middleware('auth:api')->group(function () {
+
+    Route::get('/coches', [CocheController::class, 'index']);
+    Route::post('/coches', [CocheController::class, 'store']);
+    Route::get('/coches/{id}', [CocheController::class, 'show']);
+    Route::put('/coches/{id}', [CocheController::class, 'update']);
+    Route::delete('/coches/{id}', [CocheController::class, 'destroy']);
+
+
+    Route::post("register", [AuthController::class, 'register']);
+    Route::post("logout", [AuthController::class, 'logout']);
+});
