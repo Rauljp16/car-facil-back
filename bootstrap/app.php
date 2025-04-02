6c7d2a3d383b6716d5628ff45277c45bde1dd50d
Rauljp16 <raul_astra16@hotmail.com>
2025-04-02 18:55:06 +0200

    refactor cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Middleware\HandleCors; // Importar el middleware de CORS
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(HandleCors::class); // Agregar CORS a los middleware
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
