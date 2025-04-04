6e364321f72f75a580e9d640b2f72dfb20759533
Rauljp16 <raul_astra16@hotmail.com>
2025-04-04 10:46:51 +0200

    cambios para middlewarwa para funcionar en producción

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post("login", [AuthController::class, 'login']);
+Route::post("register", [AuthController::class, 'register']);
 Route::get('/coches', [CocheController::class, 'index']);
 Route::get('/coches/{id}', [CocheController::class, 'show']);
 
@@ -19,7 +20,6 @@ Route::middleware('auth:api')->group(function () {
 
     Route::post('/coches/{cocheId}/images', [ImageController::class, 'store']);
 
-    Route::post("register", [AuthController::class, 'register']);
     Route::post("logout", [AuthController::class, 'logout']);
 
 });
